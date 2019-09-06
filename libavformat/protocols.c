767b3ea8d4708016c5fe59f059aeab4fd15f9606
hongjucheng <hongjucheng_17@163.com>
2019-09-06 17:52:16 +0800

    删除flvseg

--- libavformat/protocols.c
+++ libavformat/protocols.c
@@ -69,7 +69,6 @@ extern const URLProtocol ff_libsrt_protocol;
 extern const URLProtocol ff_libssh_protocol;
 extern const URLProtocol ff_libsmbclient_protocol;
 extern const URLProtocol ff_libzmq_protocol;
-extern const URLProtocol ff_flvseg_protocol;
 
 #include "libavformat/protocol_list.c"
 
